6cb6cf9b3f64a85933c499b613844e7285839c3d
Ak4shKr <akashkmr7667@gmail.com>
2025-03-26 23:20:57 +0530

    project setup with react vite

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
+  );
+};
